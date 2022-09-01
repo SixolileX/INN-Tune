dd2218f35df36ee49d9bfa39f828aa47bec40cb0
SixolileX <108371034+SixolileX@users.noreply.github.com>
2022-09-01 13:47:14 +0200

    first commit

---
+++ InnSystem/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using InnSystem.Areas.Identity.Data;
+using InnSystem.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InnSystem.Areas.Identity.IdentityHostingStartup))]
+namespace InnSystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<InnSystemContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("InnSystemContextConnection")));
+
+                services.AddDefaultIdentity<InnSystemUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<InnSystemContext>();
+            });
+        }
+    }
+}
---
+++ InnSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InnSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
